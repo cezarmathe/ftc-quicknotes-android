9ec23a65a30e42a938cda1dec5d53fa7f15a1686
Cezar Mathe <cezarmathe@gmail.com>
2018-03-01 22:13:29 +0200

    small boilerplate added

---
+++ app/src/main/java/com/vectron_ftc/ftcquicknotes/DataHolders/Draft.java
@@ -0,0 +1,8 @@
+package com.vectron_ftc.ftcquicknotes.DataHolders;
+
+/**
+ * Created by cezar on 3/1/18.
+ */
+
+public class Draft {
+}
--- app/src/main/java/com/vectron_ftc/ftcquicknotes/Hub.java
+++ app/src/main/java/com/vectron_ftc/ftcquicknotes/Hub.java
@@ -2,9 +2,11 @@ package com.vectron_ftc.ftcquicknotes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.vectron_ftc.ftcquicknotes.DataHolders.HubData;
+import com.vectron_ftc.ftcquicknotes.DataHolders.Screen;
 
 public class Hub extends AppCompatActivity {
+
+    private Screen currentScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
