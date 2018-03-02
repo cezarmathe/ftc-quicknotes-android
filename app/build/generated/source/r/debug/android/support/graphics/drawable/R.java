b353a65ebab93565b44209554b6d32881b8aacb9
Cezar Mathe <cezarmathe@gmail.com>
2018-03-02 20:58:23 +0200

    worked a lot on the hub class

--- app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
+++ app/build/generated/source/r/debug/android/support/graphics/drawable/R.java
@@ -69,49 +69,49 @@ public final class R {
         public static final int action_image = 0x7f080011;
         public static final int action_text = 0x7f080017;
         public static final int actions = 0x7f080018;
-        public static final int async = 0x7f08001e;
-        public static final int blocking = 0x7f080022;
-        public static final int chronometer = 0x7f08002b;
-        public static final int forever = 0x7f080048;
-        public static final int icon = 0x7f08004c;
-        public static final int icon_group = 0x7f08004d;
-        public static final int info = 0x7f080050;
-        public static final int italic = 0x7f080051;
-        public static final int line1 = 0x7f080055;
-        public static final int line3 = 0x7f080056;
-        public static final int normal = 0x7f080062;
-        public static final int notification_background = 0x7f080063;
-        public static final int notification_main_column = 0x7f080064;
-        public static final int notification_main_column_container = 0x7f080065;
-        public static final int right_icon = 0x7f080070;
-        public static final int right_side = 0x7f080071;
-        public static final int text = 0x7f08009a;
-        public static final int text2 = 0x7f08009b;
-        public static final int time = 0x7f0800a1;
-        public static final int title = 0x7f0800a2;
+        public static final int async = 0x7f08001f;
+        public static final int blocking = 0x7f080024;
+        public static final int chronometer = 0x7f08002d;
+        public static final int forever = 0x7f08004c;
+        public static final int icon = 0x7f080054;
+        public static final int icon_group = 0x7f080055;
+        public static final int info = 0x7f080058;
+        public static final int italic = 0x7f080059;
+        public static final int line1 = 0x7f08005d;
+        public static final int line3 = 0x7f08005e;
+        public static final int normal = 0x7f08006a;
+        public static final int notification_background = 0x7f08006b;
+        public static final int notification_main_column = 0x7f08006c;
+        public static final int notification_main_column_container = 0x7f08006d;
+        public static final int right_icon = 0x7f08007a;
+        public static final int right_side = 0x7f08007b;
+        public static final int text = 0x7f0800a7;
+        public static final int text2 = 0x7f0800a8;
+        public static final int time = 0x7f0800ae;
+        public static final int title = 0x7f0800af;
     }
     public static final class integer {
         public static final int status_bar_notification_info_maxnum = 0x7f090009;
     }
     public static final class layout {
-        public static final int notification_action = 0x7f0a002a;
-        public static final int notification_action_tombstone = 0x7f0a002b;
-        public static final int notification_template_custom_big = 0x7f0a0032;
-        public static final int notification_template_icon_group = 0x7f0a0033;
-        public static final int notification_template_part_chronometer = 0x7f0a0037;
-        public static final int notification_template_part_time = 0x7f0a0038;
+        public static final int notification_action = 0x7f0a002b;
+        public static final int notification_action_tombstone = 0x7f0a002c;
+        public static final int notification_template_custom_big = 0x7f0a0033;
+        public static final int notification_template_icon_group = 0x7f0a0034;
+        public static final int notification_template_part_chronometer = 0x7f0a0038;
+        public static final int notification_template_part_time = 0x7f0a0039;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0c0029;
+        public static final int status_bar_notification_info_overflow = 0x7f0d0032;
     }
     public static final class style {
-        public static final int TextAppearance_Compat_Notification = 0x7f0d0100;
-        public static final int TextAppearance_Compat_Notification_Info = 0x7f0d0101;
-        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f0d0103;
-        public static final int TextAppearance_Compat_Notification_Time = 0x7f0d0106;
-        public static final int TextAppearance_Compat_Notification_Title = 0x7f0d0108;
-        public static final int Widget_Compat_NotificationActionContainer = 0x7f0d017e;
-        public static final int Widget_Compat_NotificationActionText = 0x7f0d017f;
+        public static final int TextAppearance_Compat_Notification = 0x7f0e0100;
+        public static final int TextAppearance_Compat_Notification_Info = 0x7f0e0101;
+        public static final int TextAppearance_Compat_Notification_Line2 = 0x7f0e0103;
+        public static final int TextAppearance_Compat_Notification_Time = 0x7f0e0106;
+        public static final int TextAppearance_Compat_Notification_Title = 0x7f0e0108;
+        public static final int Widget_Compat_NotificationActionContainer = 0x7f0e017e;
+        public static final int Widget_Compat_NotificationActionText = 0x7f0e017f;
     }
     public static final class styleable {
         public static final int[] FontFamily = { 0x7f030091, 0x7f030092, 0x7f030093, 0x7f030094, 0x7f030095, 0x7f030096 };
--- app/src/main/java/com/vectron_ftc/ftcquicknotes/Hub.java
+++ app/src/main/java/com/vectron_ftc/ftcquicknotes/Hub.java
@@ -1,13 +1,21 @@
 package com.vectron_ftc.ftcquicknotes;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
 import com.vectron_ftc.ftcquicknotes.DataHolders.Screen;
+import com.vectron_ftc.ftcquicknotes.Helpers.PreferencesHelper;
 
 public class Hub extends AppCompatActivity {
 
     private Screen currentScreen;
+    private DrawerLayout hubDrawer;
+    private PreferencesHelper preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,105 @@ public class Hub extends AppCompatActivity {
         currentScreen = Screen.getScreenFromString(intent.getStringExtra("screen"));
 
         setContentView(R.layout.activity_hub);
+
+//        initialize preferences
+        preferences = new PreferencesHelper(getApplicationContext());
+//        handling navigation bar
+        hubDrawer = findViewById(R.id.hub_drawer);
+        NavigationView hubNavigation = findViewById(R.id.hub_navigation);
+        hubNavigation.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                         set item as selected to persist highlight
+                        menuItem.setChecked(true);
+//                         close drawer when item is tapped
+                        hubDrawer.closeDrawers();
+//                        set current screen from the selected menu
+                        int menuId = menuItem.getItemId();
+                        if (menuId != R.id.settings) {
+                            currentScreen = setScreenFromMenuItem(menuId);
+                            saveScreen(currentScreen.toString());
+                        }
+                        else {
+                            saveScreen(currentScreen.toString());
+                            launchSettings();
+                        }
+                        return true;
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        saveScreen(currentScreen.toString());
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        saveScreen(currentScreen.toString());
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        // TODO: 3/2/18 add resume screen method: reload the last screen
+        getScreen();
+        reload();
+        super.onResume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+//        View v
+        hubDrawer = findViewById(R.id.hub_drawer);
+        if (hubDrawer.isDrawerOpen(R.id.hub_drawer)) {
+            hubDrawer.closeDrawer(R.id.hub_drawer);
+        }
+    }
+
+    public Screen setScreenFromMenuItem(int menuId) {
+        switch (menuId) {
+            case R.id.drafts:
+                return Screen.DRAFTS;
+            case R.id.finished_things:
+                return Screen.FINISHED_THINGS;
+            case R.id.quick_notes:
+                return Screen.QUICK_NOTES;
+            case R.id.pic_bundles:
+                return Screen.BUNDLES_OF_PICTURES;
+            case R.id.spy_reports:
+                return Screen.SPY_REPORTS;
+            case R.id.future_plans_activities:
+                return Screen.FUTURE_PLANS_ACTIVITIES;
+            case R.id.archive:
+                return Screen.ARCHIVE;
+            case R.id.tags:
+                return Screen.TAGS;
+            case R.id.bin:
+                return Screen.BIN;
+            default:
+                return Screen.DRAFTS;
+        }
+    }
+
+    public void saveScreen(String value) {
+        preferences.writeString("screen", value);
+    }
+
+    public void getScreen() {
+        currentScreen = Screen.getScreenFromString(preferences.readString("screen"));
+    }
+
+    public void launchSettings() {
+        // TODO: 3/2/18 replace Splash.class with Settings.class
+        Intent launchSettings = new Intent(Hub.this, Splash.class);
+        startActivity(launchSettings);
+    }
+
+    public void reload() {
+        // TODO: 3/2/18 write function for reloading list items according to current screen
     }
 }
